ec3fd709d7376a6d1501cf3223c25b45561d6168
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-07-03 14:15:22 +0100

    Fix ImageCell impor t error

--- src/components/data-table/Columns.tsx
+++ src/components/data-table/Columns.tsx
@@ -2,7 +2,7 @@ import { ColumnDef } from "@tanstack/react-table";
 
 import { Checkbox } from "../ui/checkbox";
 import { Product } from "typings";
-import ImageCell from "./ImageCEll";
+import ImageCell from "./ImageCell.tsx";
 
 export const columns: ColumnDef<Product>[] = [
 	{
--- src/components/data-table/ImageCell.tsx
+++ src/components/data-table/ImageCell.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import TableImage from "../../assets/TableImage.png";
 
 const ImageCell = ({ value, sku }: { value: string; sku: string }) => {
