d22457fa4c5463525c27a0bc93697aeaba7a8dad
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-07-03 13:29:38 +0100

    Create ImageCell Component for rendering product images in the table

---
+++ src/components/data-table/ImageCell.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import TableImage from "../../assets/TableImage.png";
+
+const ImageCell: React.FC<{ value: string; sku: string }> = ({
+	value,
+	sku,
+}) => {
+	const [imgSrc, setImgSrc] = useState(TableImage);
+
+	useEffect(() => {
+		const img = new Image();
+		img.src = value;
+
+		img.onload = () => setImgSrc(value);
+		img.onerror = () => setImgSrc(TableImage);
+
+		return () => {
+			img.onload = null;
+			img.onerror = null;
+		};
+	}, [value]);
+
+	return (
+		<img
+			style={{ width: "40px", height: "40px" }}
+			src={imgSrc}
+			alt={sku}
+		/>
+	);
+};
+
+export default ImageCell;
